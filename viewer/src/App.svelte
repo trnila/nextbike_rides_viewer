a7e6b840d2e8bc5e00e05d4978e229e5e53cec14
Daniel Trnka <daniel.trnka@gmail.com>
2022-07-30 17:56:36 +0200

    bike icon for marker

--- viewer/src/App.svelte
+++ viewer/src/App.svelte
@@ -11,6 +11,13 @@
 
   let M;
 
+  const icon = L.divIcon({
+    html: `<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="30" height="30" preserveAspectRatio="xMidYMid meet" viewBox="0 0 48 48"><path fill="currentColor" fill-rule="evenodd" d="m35.745 12.17l-4.925 1.48l3.28 8.578a8 8 0 1 1-1.868.715l-1.648-4.31l-5.682 11.802A1 1 0 0 1 24 31h-4.062A8.001 8.001 0 0 1 4 30a8 8 0 0 1 15.938-1h2.5l-4.88-13.664A.998.998 0 0 1 17.5 15H16a1 1 0 1 1 0-2h4.5a1 1 0 1 1 0 2h-.938l1.842 5.157l8.127-4.277l-.965-2.523a1 1 0 0 1 .647-1.315l5.957-1.787l.575 1.915Zm-13.662 9.89l1.972 5.52l4.23-8.784l-6.202 3.264Zm12.983 8.297l-2.113-5.527a6 6 0 1 0 1.868-.715l2.113 5.528a1 1 0 0 1-1.868.714ZM17.917 29H12a1 1 0 1 0 0 2h5.917A6.002 6.002 0 0 1 6 30a6 6 0 0 1 11.917-1Z" clip-rule="evenodd"/></svg>`,
+    className: "",
+    iconSize: [30, 30],
+    iconAnchor: [15, 15],
+  });
+
   function map(container) {
     var map = L.map(container).setView([49.820923, 18.262524], 13);
     M = map;
@@ -87,7 +94,7 @@
       }
 
       if(t.tsDst > current && !active.find((el) => el.index == head)) {
-        let marker = L.marker([t.srcLat, t.srcLng]).addTo(M);
+        let marker = L.marker([t.srcLat, t.srcLng], {icon: icon}).addTo(M);
         var polyline = L.polyline([[t.srcLat, t.srcLng],[t.dstLat, t.dstLng]], {color: 'red'}).addTo(M);
 
         active.push({
