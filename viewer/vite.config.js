e493703e76d5fcdd76de261e34492ebe6e4fcdc0
Daniel Trnka <daniel.trnka@gmail.com>
2026-05-27 00:17:29 +0200

    viewer: build self-contained index.html

--- viewer/vite.config.js
+++ viewer/vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    svelte(),
+    mode !== "development" && viteSingleFile(),
+  ],
   server: {
     proxy: {
       '/rides': 'http://localhost:8080/',
       '/stations.json': 'http://localhost:8080/',
     }
   }
-})
+}))
